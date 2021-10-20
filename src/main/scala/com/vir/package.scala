a87f4f6ddfb9aa1dc162c42bd57f35066fac4a27
Yuldasheff <maxyuldasheff@gmail.com>
2021-10-20 10:58:17 +0500

    added blaze server

--- src/main/scala/com/vir/package.scala
+++ src/main/scala/com/vir/package.scala
@@ -1,18 +1,45 @@
 package com
 
-import cats._
-import cats.effect._
-import cats.implicits._
-import org.http4s.circe._
-import org.http4s._
-import io.circe.generic.auto._
-import io.circe.syntax._
-import org.http4s.dsl._
-import org.http4s.dsl.impl._
-import org.http4s.headers._
-import org.http4s.implicits._
-import org.http4s.server._
+import org.http4s.*
+import org.http4s.blaze.server.BlazeServerBuilder
+import org.http4s.server.Router
+import cats.*
+import cats.effect.*
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits.*
+import org.http4s.circe.*
+import org.http4s.*
+import io.circe.generic.auto.*
+import io.circe.syntax.*
+import org.http4s.dsl.*
+import org.http4s.dsl.impl.*
+import org.http4s.headers.*
+import org.http4s.implicits.*
+import org.http4s.server.*
+
+import scala.concurrent.ExecutionContext
+
 
 package object vir {
 
-}
+  class PingApi() extends Http4sDsl[IO]{
+    val routes = HttpRoutes.of[IO]{
+      case GET -> Root / "ping" => Ok("pong")
+    }
+  }
+
+  object PingApi extends IOApp{
+    val httpApp = Router(
+      "/" -> new PingApi().routes
+    ).orNotFound
+
+
+
+  override def run(args: List[String]): IO[ExitCode] = stream(args).compile.drain.as(ExitCode.Success)
+
+  private def stream(value: List[String]): fs2.Stream[IO, ExitCode] =
+    BlazeServerBuilder[IO](ExecutionContext.global)
+      .bindHttp(8000, "0.0.0.0")
+      .withHttpApp(httpApp)
+      .serve
+}}
