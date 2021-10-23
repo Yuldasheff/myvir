e9c0a7567d8dba34fd20e62dae3925132c3f9470
Yuldasheff <maxyuldasheff@gmail.com>
2021-10-23 19:45:09 +0500

    added models and case classes

--- src/main/scala/com/vir/Models.scala
+++ src/main/scala/com/vir/Models.scala
@@ -1,5 +1,9 @@
 package com
 
+import com.Models.WriterModels.{Id, Email, Pass, Name, Status, OrdersCompleted, TotalEarned}
+import com.Models.OrderModels.{Id, Title, WordCount, NOHeadings, Status, NOImages, NOParagraphs, Comments, Plagiarism}
+import com.Models.AssignmentModels.{Id, OrderId, WriterId, CompletedAt, DueDate, CreatedAt}
+
 import java.util.{Date, UUID}
 
 object Models {
@@ -40,5 +44,9 @@ object Models {
     type CompletedAt = Date
 
   }
+
+  case class Writer(id: Id, email: Email, pass: Pass, name: Name, status: Status, ordersCompleted: OrdersCompleted, totalEarned: TotalEarned)
+  case class Order(id: Id, title: Title, status: Status, wordCount: WordCount, NOHeadings: NOHeadings, NOImages: NOImages, NOParagraphs: NOParagraphs, comments: Comments, plagiarism: Plagiarism)
+  case class Assignment(id: Id, writerId: WriterId, orderId: OrderId, createdAt: CreatedAt, dueDate: DueDate, completedAt: CompletedAt)
 
 }
